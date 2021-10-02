7c7acd1bdcb49185f882749604b90c78614de29f
Hattorim <sozw.com@outlook.com>
2021-10-02 15:27:09 +0900

    first commit

---
+++ 20211002hattori/js/main.js
@@ -0,0 +1,6 @@
+const target = document.getElementById("menu");
+target.addEventListener('click', () => {
+  target.classList.toggle('open');
+  const nav = document.getElementById("drawer__nav");
+  nav.classList.toggle('in');
+});
